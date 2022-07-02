817262aa4d03f42aa5bb2dfd6aacfc03851ac8d6
Rohit Man Shrestha <stha.rht028@gmail.com>
2022-07-03 00:06:11 +0545

    feat: random color generator

---
+++ pages/api/get-random-colors.ts
@@ -0,0 +1,67 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+interface RGBColor {
+  red: number;
+  green: number;
+  blue: number;
+  type: string;
+}
+
+interface HSLColor {
+  hue: number;
+  saturation: number;
+  lightness: number;
+  type: string;
+}
+
+interface Handler<T> {
+  Encode(color: any): T
+}
+
+class RGB implements Handler<RGBColor> {
+  Encode(color: any): RGBColor {
+    return {
+      blue: getRandomNumbers(0,255),
+      green: getRandomNumbers(0,255),
+      red: getRandomNumbers(0,255),
+      type: 'rgb'
+    }
+  }
+}
+
+class HSL implements Handler<HSLColor> {
+  Encode(color: any): HSLColor {
+    return {
+      hue: getRandomNumbers(1,360),
+      saturation: getRandomNumbers(0,100),
+      lightness: getRandomNumbers(0,100),
+      type: 'hsl',
+      
+    }
+  }
+}
+
+class Controller {
+  private handlers: Handler<any>[] = [new RGB(), new HSL()];
+  getResponse = () => {
+    return getRandomColors(5).map(x => {
+      return this.handlers[getRandomNumbers(0, this.handlers.length)].Encode(x);
+    })
+  }
+}
+
+const getRandomNumbers = (min: number, max: number) => Math.floor(Math.random() * max + min);
+
+const getRandomColors = (count: number) => {
+  return Array(count).fill('');
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const controller = new Controller();
+  
+  res.status(200).json(controller.getResponse())
+}
