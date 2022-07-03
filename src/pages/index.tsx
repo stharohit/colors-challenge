93cba7ebb252fcd793ba8d21692b1b6a167fa521
Rohit Man Shrestha <stha.rht028@gmail.com>
2022-07-03 11:27:27 +0545

    feat: imported home page in Index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,54 +1,11 @@
-import { TColorSpace } from '@/types/color-space';
 import type { NextPage } from 'next'
-import { useEffect, useState } from 'react'
-import styles from '@/styles/Home.module.css'
-import { isHSL, isRGB } from '@/utils/checkColorSpace';
+import Home from './home';
 
-const Home: NextPage = () => {
-
-  const [colors, setcolors] = useState<Array<TColorSpace>>();
-
-  const callHello = () => {
-    fetch('/api/get-random-colors').then(res => res.json()).then(res => setcolors(res))
-  }
-
-  const generateColors = () => {
-    callHello()
-  }
-
-  useEffect(() => {
-
-    callHello()
-  }, [])
-
+const Index: NextPage = () => {
 
   return (
-    <div className={styles.container}>
-      <div className={styles.swatchPanel}>
-        {mapColors(colors)}
-      </div>
-      <button className={styles.generateBtn} onClick={generateColors}>Generate Colors</button>
-    </div>
+    <Home />
   )
 }
 
-const mapColors = (colors: Array<TColorSpace> | undefined) => {
-
-  const getColorMap = (color: TColorSpace) => {
-    if (isRGB(color)) {
-      return `rgb(${color.red}, ${color.blue}, ${color.green})`
-    }
-
-    if (isHSL(color)) {
-      return `hsl(${color.hue}, ${color.saturation}%, ${color.lightness}%)`
-    }
-  }
-
-  if (!colors) return null;
-
-  return colors.map(color => (
-    <div className={styles.colorBox} style={{ background: getColorMap(color) }}></div>
-  ))
-}
-
-export default Home
+export default Index
