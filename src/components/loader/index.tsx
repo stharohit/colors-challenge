2a8f8abfb87c144ceb55331502bb46f22ffa1872
Rohit Man Shrestha <stha.rht028@gmail.com>
2022-07-03 11:25:42 +0545

    feat: added simple loader

---
+++ src/components/loader/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './loader.module.css';
+
+const Loader = () => {
+  return (
+    <div className={styles.loaderContainer}>Loading . . .</div>
+  )
+}
+
+export default Loader
